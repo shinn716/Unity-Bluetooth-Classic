492ce081f73072a5a20537f737bceaaec31a4791
John Tsai <shinn716@livemail.tw>
2021-09-03 17:03:12 +0800

    Add files via upload

---
+++ Unity/BluetoothSample.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BluetoothSample : MonoBehaviour
+{
+    SerialManager serialReceiver;
+
+    public string port = "COM8";
+    public int baudrate = 115200;
+
+    public string message = "0";
+
+    void Start()
+    {
+        serialReceiver = new SerialManager(port, baudrate);
+        serialReceiver.callback = GetData;
+    }
+
+    [ContextMenu("Send")]
+    void Send()
+    {
+        serialReceiver.SendMessage(message);
+    }
+
+    private void GetData()
+    {
+        string data = serialReceiver.stringData;
+        Debug.Log("[Log]" + data);
+    }
+
+    private void OnDisable()
+    {
+        if (serialReceiver != null)
+            serialReceiver.Dispose();
+    }
+}
